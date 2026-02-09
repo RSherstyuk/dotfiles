dca1f93f0b53148df09491e169bd490314de093a
RSherstyuk <rus.sherstyuk@gmail.com>
2026-02-09 18:17:04 +0300

    feat: add options and comment

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,42 +1,3 @@
--- -- ==============================
--- -- Общие настройки
--- -- ==============================
-vim.opt.mouse = "a"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-vim.opt.number = true
-vim.opt.cursorline = false
-vim.opt.swapfile = false
-vim.opt.scrolloff = 7
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.fileformat = "unix"
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.termguicolors = true
-vim.opt.relativenumber = true
-vim.opt.clipboard = "unnamedplus"
-
--- ==============================
-vim.g.clipboard = {
-	name = "win32yank",
-
-	copy = {
-		["+"] = "/home/russh/bin/win32yank.exe -i --crlf",
-		["*"] = "/home/russh/bin/win32yank.exe -i --crlf",
-	},
-	paste = {
-		["+"] = "/home/russh/bin/win32yank.exe -o --lf",
-		["*"] = "/home/russh/bin/win32yank.exe -o --lf",
-	},
-	cache_enabled = 0,
-}
--- ==============================
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -69,9 +30,7 @@ require("lazy").setup({
 	},
 })
 
--- ==============================
--- Keymaps
--- ==============================
+require("config.opt")
 require("config.keymaps")
 
 local api = vim.api
@@ -81,12 +40,10 @@ api.nvim_set_keymap(
 	":Neotree toggle<CR>",
 	{ noremap = true, silent = true, desc = "Toggle Neo-tree" }
 )
-
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 vim.o.foldlevel = 99
 vim.o.foldenable = true
-
 vim.cmd([[colorscheme kanagawa-dragon]])
 
 -- ==============================
